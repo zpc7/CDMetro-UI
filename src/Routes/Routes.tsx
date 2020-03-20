f756fcca93504211d6636a8c334a6ccb6a81bd0e
zpc7 <zpc.excel@foxmail.com>
2020-03-20 11:59:46 +0800

    处理侧边栏

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,17 +1,15 @@
 import React from 'react'
-import { Layout, Menu } from 'antd'
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
-import { BarChartOutlined, SolutionOutlined, UserOutlined, ToolOutlined } from '@ant-design/icons'
+import { Layout } from 'antd'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
+import Sidebar from './SideBar'
 import PageTest from '@/Pages/demoTest/DemoTest'
 import PageOverView from '@/Pages/overView/OverView'
 import PagePassengerAmountManage from '@/Pages/manage/PassengerAmountManage'
 
 import './Routes.less'
-import logoImg from '@/Static/images/logo.png'
 
-const { SubMenu } = Menu
-const { Header, Sider, Content } = Layout
+const { Header, Content } = Layout
 
 export default class Routes extends React.Component {
 
@@ -19,55 +17,15 @@ export default class Routes extends React.Component {
     return (
       <BrowserRouter>
         <Layout id='app-wrapper'>
-          <Sider collapsible>
-            <div className="logo">
-              <img src={logoImg} />
-            </div>
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={['overView']}>
-              <Menu.Item key="overView">
-                <Link to='/'>
-                  <BarChartOutlined />
-                  <span>OverView</span>
-                </Link>
-              </Menu.Item>
-              <SubMenu
-                key="management"
-                title={
-                  <span>
-                    <SolutionOutlined />
-                    <span>Manage</span>
-                  </span>
-                }
-              >
-                <Menu.Item key="dataManage">
-                  <Link to='/manage'>
-                    <UserOutlined />
-                    <span>客运数据管理</span>
-                  </Link>
-                </Menu.Item>
-                <Menu.Item key="config">
-                  <Link to='/config'>
-                    <ToolOutlined />
-                    <span>线路配置</span>
-                  </Link>
-                </Menu.Item>
-              </SubMenu>
-              <Menu.Item key="test">
-                <Link to='/test'>
-                  <BarChartOutlined />
-                  <span>Test</span>
-                </Link>
-              </Menu.Item>
-            </Menu>
-          </Sider>
+          <Sidebar />
           <Layout>
             <Header id='header'>
             </Header>
             <Content id='content'>
               <Switch>
                 <Route exact path="/" component={PageOverView} />
+                <Route exact path="/dataManage" component={PagePassengerAmountManage} />
                 <Route exact path="/test" component={PageTest} />
-                <Route exact path="/manage" component={PagePassengerAmountManage} />
               </Switch>
             </Content>
           </Layout>
---
+++ src/Routes/Sidebar.tsx
@@ -0,0 +1,85 @@
+import React, { Component } from 'react'
+import { Layout, Menu } from 'antd'
+import { Link, withRouter } from 'react-router-dom'
+import { BarChartOutlined, UserOutlined, ToolOutlined } from '@ant-design/icons'
+import { RouteComponentProps } from 'react-router'
+
+const { Sider } = Layout
+import logoImg from '@/Static/images/logo.png'
+
+interface State {
+  collapsed: boolean
+  selectedKey: string
+}
+
+class Sidebar extends Component<RouteComponentProps, State> {
+  private historySubscription
+  state = {
+    collapsed: false,
+    selectedKey: '',
+  }
+
+  onCollapse = (collapsed: boolean) => {
+    this.setState({ collapsed })
+  }
+
+  updateSidebarMenu = location => {
+    const pathname = location.pathname.split('/')[1]
+    const selectedKey = pathname === '' ? 'overView' : pathname
+
+    this.setState({ selectedKey })
+  }
+
+  componentDidMount() {
+    const { history, location } = this.props
+    this.updateSidebarMenu(location)
+    this.historySubscription = history.listen(location => this.updateSidebarMenu(location))
+  }
+
+  componentWillUnmount() {
+    this.historySubscription()
+  }
+
+  render() {
+    const { collapsed, selectedKey } = this.state
+    return (
+      <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
+        <div className="logo">
+          <img src={logoImg} />
+        </div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          selectedKeys={[selectedKey]}
+        >
+          <Menu.Item key="overView">
+            <Link to='/'>
+              <BarChartOutlined />
+              <span>OverView</span>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="dataManage">
+            <Link to='/dataManage'>
+              <UserOutlined />
+              <span>客运数据管理</span>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="config">
+            <Link to='/config'>
+              <ToolOutlined />
+              <span>线路配置</span>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="test">
+            <Link to='/test'>
+              <BarChartOutlined />
+              <span>Test</span>
+            </Link>
+          </Menu.Item>
+        </Menu>
+      </Sider >
+    )
+  }
+}
+
+export default withRouter(Sidebar)
