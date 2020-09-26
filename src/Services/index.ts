dedaf92f2b39db62076feacc28f61ef95e8eaa4e
zpc7 <zpc.excel@foxmail.com>
2020-09-26 10:18:09 +0800

    upgrade package and separate interface from service

--- src/Services/index.ts
+++ src/Services/index.ts
@@ -1,59 +1,7 @@
 import http from '@/Utils/http'
+import { LineConfigResponse, PassengerTrafficResponse, PassengerTrafficItem, AverageMonthlyDataResponse, AverageMonthlyDataWithDateTypeResponse, HighestRecordResponse, LineConfigItem } from './interface'
 
-export interface LineConfigItem {
-  id?: number // 兼容 新增和获取
-  lineNumber: string
-  lineColor: string
-  lineType: string
-  openDate: string
-}
-interface LineConfigResponse {
-  total: number
-  list: LineConfigItem[]
-}
 
-interface LineDataItem {
-  lineId: number
-  lineAmount: string
-}
-export interface PassengerTrafficItem {
-  id?: number // 兼容 新增和获取
-  date: string
-  dateType: string
-  lineData: LineDataItem[]
-  sum: string
-}
-interface PassengerTrafficResponse {
-  total: number
-  list: PassengerTrafficItem[]
-}
-interface MonthlyAverageData {
-  average: string
-  lineAverage: Array<{ lineId: number; average: string }>
-}
-export interface AverageMonthlyDataResponse {
-  max: {
-    date: string
-    value: string
-  }
-  min: {
-    date: string
-    value: string
-  }
-  currentMonth: MonthlyAverageData
-  lastMonth: MonthlyAverageData
-  sameMonthLastYear: MonthlyAverageData
-}
-export interface AverageMonthlyDataWithDateTypeResponse {
-  NWD: MonthlyAverageData  // 普通工作日
-  TDBH: MonthlyAverageData  // 假期前一天
-  SH: MonthlyAverageData // 周末和法定节假日
-}
-export interface HighestRecordResponse {
-  max: string
-  maxDate: string
-  lineMax: Array<{ lineId: number; value: string; date: string }>
-}
 // 获取线路配置列表
 export const getLineConfig: () => Promise<LineConfigResponse> = () => http.get('/lineConfig')
 // 获取完整客运量列表
